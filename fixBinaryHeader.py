387f68f7d857996b3ee58dfeb672192829ffcbd4
trndr <trndr.k@gmail.com>
2013-11-06 15:56:50 +0000

    Added support for ARV7510PW

--- fixBinaryHeader.py
+++ fixBinaryHeader.py
@@ -11,7 +11,8 @@ def writeAsWord(number, array, ofset):
     tmp=number&0xFF
     array[(ofset+3-i)]=tmp
     number = number>>8
-stupidMagicArray=[0x70581e8, 0xff7411e3, 0x8373b619, 0xa1ef5492]
+stupidMagicArray={0x00A51409:[0x070581e8, 0xff7411e3, 0x8373b619, 0xa1ef5492],
+                  0x00A52004:[0xc7bc80da, 0xea320881, 0x18a21be1, 0x484e3b0c]}
 
 if (len(sys.argv)>1):
   fileInputName=sys.argv[1]
@@ -24,16 +25,22 @@ if (len(sys.argv)>1):
   writeAsWord(0x4E415742, fileBytes, 0x408)     #Unknown but has to be this or will trigger an corrupted firmware triger
   writeAsWord(0x00000000, fileBytes, 0x410)     #Can be anything, but for md5 must be this
   writeAsWord(len(fileBytes), fileBytes, 0x40C) #Set file size
-  writeAsWord(0x00A51409, fileBytes, 0x414)     #Set product code
   writeAsWord(0x00000001, fileBytes, 0x418)     #Set revision
-
+  
+  magicArray=[]
+  try: 
+    magicArray=stupidMagicArray[readAsWord(fileBytes, 0x414)]
+  except:
+    print("Unknown Product code")
+    print(hex(readAsWord(fileBytes, 0x414)))
+    exit()
 
 #  MD5inFile=fileBytes[0xFF0:0x1000]
   MD5ofFile=hashlib.md5(fileBytes[:0xFF0]+fileBytes[0x1000:]).digest()
 
   for i in range(4):
-    writeAsWord(stupidMagicArray[i]^readAsWord(MD5ofFile, i*4), fileBytes, 0xFF0+i*4)
-  fileOut=open(fileInputName[:-4]+"cool.bin", 'wb')
+    writeAsWord(magicArray[i]^readAsWord(MD5ofFile, i*4), fileBytes, 0xFF0+i*4)
+  fileOut=open(fileInputName[:-4]+"-fixed-header.bin", 'wb')
   fileOut.write(fileBytes)
   fileOut.close()
 
